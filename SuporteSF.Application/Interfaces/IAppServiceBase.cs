e9554e1b37c88632c9e855624846a1d420495469
gabrielmoura23 <gabrielandrade.moura@gmail.com>
2015-01-16 08:06:10 -0300

    Arquivo que ficou faltando na aplicação.

---
+++ SuporteSF.Application/Interfaces/IAppServiceBase.cs
@@ -0,0 +1,10 @@
+﻿using SuporteSF.Infra.Data.Interfaces;
+
+namespace SuporteSF.Application.Interfaces
+{
+    public interface IAppServiceBase<TContext> where TContext : IDbContext
+    {
+        void BeginTransaction();
+        void Commit();
+    }
+}
